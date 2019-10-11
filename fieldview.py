5666600603126afe51aa102d4c6f27333ec140e9
Sordamente <tomer.sedan@gmail.com>
2019-10-11 15:42:35 -0700

    Client / Flask Communication
    Allowed for basic AJAX client to flask (and vice versa) communication. Information about robot location is now transferred between the two.

--- fieldview.py
+++ fieldview.py
@@ -1,9 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
+robLocation = {"x":10,"y":20} # todo: update this variable when you recieve latest robot coords
+
+# A person wants to become a client
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html") # Fetch index.html (and all it's subfiles) and give them out
+
+# The client is updating you with its robot requested location (mouse event) information.
+@app.route('/locationpost', methods=['POST'])
+def locpost():
+    data = request.get_json()
+    print(data)
+    return "Location Updated" # Python wants you to return something
+
+# The client is asking you to give it the robot's current location information.
+@app.route('/locationget', methods=['POST'])
+def locget():
+    return jsonify(robLocation) # Send updated robot location information to the client
 
 if __name__ == '__main__':
         app.run(debug=True)
+
+pos_string = "12.2 30.2"
--- static/fieldview.js
+++ static/fieldview.js
@@ -1,12 +1,42 @@
+const req = new XMLHttpRequest();
+const postrl = "http://localhost:5000/locationpost";
+const getrl = "http://localhost:5000/locationget";
+const obj = { your : "mom" };
+
 var robot = document.getElementById("robot");
 var container = document.getElementById("contain");
 a = container.addEventListener("click", getClickPosition, false);
 console.log(a)
 function getClickPosition(e) {
-    console.log(e)
-    var xPosition = e.clientX - (robot.clientWidth / 2);
-    var yPosition = e.clientY - (robot.clientHeight / 2);
+    // console.log(e)
+    // var xPosition = e.clientX - (robot.clientWidth / 2);
+    // var yPosition = e.clientY - (robot.clientHeight / 2);
      
-    robot.style.left = xPosition + "px";
-    robot.style.top = yPosition + "px";
+    // robot.style.left = xPosition + "px";
+    // robot.style.top = yPosition + "px";
+
+    $.ajax({
+        type: "POST",
+        url: postrl,
+        contentType: "application/json",
+        data: JSON.stringify({
+            x:e.clientX,
+            y:e.clientY
+        }),
+        dataType: "json",
+        success: function(response) { console.log(response) },
+        error: function(err) { console.log(err) }
+    });
 }
+
+setInterval(() => {
+    $.ajax({
+        type: "POST",
+        url: getrl,
+        contentType: "application/json",
+        data: JSON.stringify({}),
+        dataType: "json",
+        success: function(response) { console.log(response) },
+        error: function(err) { console.log(err) }
+    });
+}, 5000);
