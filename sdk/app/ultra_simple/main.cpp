dd1fbf33700c99571a8edb5f3dbbaf02580a3055
serena li <serena.li.usa@gmail.com>
2020-01-09 21:31:00 -0800

    sockets work

--- sdk/app/ultra_simple/main.cpp
+++ sdk/app/ultra_simple/main.cpp
@@ -26,8 +26,13 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 
 #include "rplidar.h" //RPLIDAR standard sdk, all-in-one header
+
+#include "ServerSocket.h"
+#include "Socket.h"
+#include "SocketException.h"
 
 #ifndef _countof
 #define _countof(_Array) (int)(sizeof(_Array) / sizeof(_Array[0]))
@@ -81,152 +86,190 @@ void ctrlc(int)
     ctrl_c_pressed = true;
 }
 
-int main(int argc, const char * argv[]) {
-    const char * opt_com_path = NULL;
-    _u32         baudrateArray[2] = {115200, 256000};
-    _u32         opt_com_baudrate = 0;
-    u_result     op_result;
+int main(int argc, const char* argv[]) {
+   const char* opt_com_path = NULL;
+   _u32         baudrateArray[2] = { 115200, 256000 };
+   _u32         opt_com_baudrate = 0;
+   u_result     op_result;
 
-    bool useArgcBaudrate = false;
+   bool useArgcBaudrate = false;
 
-    printf("Ultra simple LIDAR data grabber for RPLIDAR.\n"
-           "Version: " RPLIDAR_SDK_VERSION "\n");
+   printf("Ultra simple LIDAR data grabber for RPLIDAR.\n"
+      "Version: " RPLIDAR_SDK_VERSION "\n");
 
-    // read serial port from the command line...
-    if (argc>1) opt_com_path = argv[1]; // or set to a fixed value: e.g. "com3" 
+   // read serial port from the command line...
+   if (argc > 1) opt_com_path = argv[1]; // or set to a fixed value: e.g. "com3" 
 
-    // read baud rate from the command line if specified...
-    if (argc>2)
-    {
-        opt_com_baudrate = strtoul(argv[2], NULL, 10);
-        useArgcBaudrate = true;
-    }
+   // read baud rate from the command line if specified...
+   if (argc > 2)
+   {
+      opt_com_baudrate = strtoul(argv[2], NULL, 10);
+      useArgcBaudrate = true;
+   }
 
-    if (!opt_com_path) {
+   if (!opt_com_path) {
 #ifdef _WIN32
-        // use default com port
-        opt_com_path = "\\\\.\\com57";
+      // use default com port
+      opt_com_path = "\\\\.\\com57";
 #elif __APPLE__
-        opt_com_path = "/dev/tty.SLAB_USBtoUART";
+      opt_com_path = "/dev/tty.SLAB_USBtoUART";
 #else
-        opt_com_path = "/dev/ttyUSB0";
+      opt_com_path = "/dev/ttyUSB0";
 #endif
-    }
+   }
 
-    // create the driver instance
-	RPlidarDriver * drv = RPlidarDriver::CreateDriver(DRIVER_TYPE_SERIALPORT);
-    if (!drv) {
-        fprintf(stderr, "insufficent memory, exit\n");
-        exit(-2);
-    }
-    
-    rplidar_response_device_info_t devinfo;
-    bool connectSuccess = false;
-    // make connection...
-    if(useArgcBaudrate)
-    {
-        if(!drv)
+   // create the driver instance
+   RPlidarDriver* drv = RPlidarDriver::CreateDriver(DRIVER_TYPE_SERIALPORT);
+   if (!drv) {
+      fprintf(stderr, "insufficent memory, exit\n");
+      exit(-2);
+   }
+
+   rplidar_response_device_info_t devinfo;
+   bool connectSuccess = false;
+   // make connection...
+   if (useArgcBaudrate)
+   {
+      if (!drv)
+         drv = RPlidarDriver::CreateDriver(DRIVER_TYPE_SERIALPORT);
+      if (IS_OK(drv->connect(opt_com_path, opt_com_baudrate)))
+      {
+         op_result = drv->getDeviceInfo(devinfo);
+
+         if (IS_OK(op_result))
+         {
+            connectSuccess = true;
+         }
+         else
+         {
+            delete drv;
+            drv = NULL;
+         }
+      }
+   }
+   else
+   {
+      size_t baudRateArraySize = (sizeof(baudrateArray)) / (sizeof(baudrateArray[0]));
+      for (size_t i = 0; i < baudRateArraySize; ++i)
+      {
+         if (!drv)
             drv = RPlidarDriver::CreateDriver(DRIVER_TYPE_SERIALPORT);
-        if (IS_OK(drv->connect(opt_com_path, opt_com_baudrate)))
-        {
+         if (IS_OK(drv->connect(opt_com_path, baudrateArray[i])))
+         {
             op_result = drv->getDeviceInfo(devinfo);
 
-            if (IS_OK(op_result)) 
+            if (IS_OK(op_result))
             {
-                connectSuccess = true;
+               connectSuccess = true;
+               break;
             }
             else
             {
-                delete drv;
-                drv = NULL;
+               delete drv;
+               drv = NULL;
             }
-        }
-    }
-    else
-    {
-        size_t baudRateArraySize = (sizeof(baudrateArray))/ (sizeof(baudrateArray[0]));
-        for(size_t i = 0; i < baudRateArraySize; ++i)
-        {
-            if(!drv)
-                drv = RPlidarDriver::CreateDriver(DRIVER_TYPE_SERIALPORT);
-            if(IS_OK(drv->connect(opt_com_path, baudrateArray[i])))
-            {
-                op_result = drv->getDeviceInfo(devinfo);
+         }
+      }
+   }
+   if (!connectSuccess) {
 
-                if (IS_OK(op_result)) 
-                {
-                    connectSuccess = true;
-                    break;
-                }
-                else
-                {
-                    delete drv;
-                    drv = NULL;
-                }
-            }
-        }
-    }
-    if (!connectSuccess) {
-        
-        fprintf(stderr, "Error, cannot bind to the specified serial port %s.\n"
-            , opt_com_path);
-        goto on_finished;
-    }
+      fprintf(stderr, "Error, cannot bind to the specified serial port %s.\n"
+         , opt_com_path);
+      goto on_finished;
+   }
 
-    // print out the device serial number, firmware and hardware version number..
-    printf("RPLIDAR S/N: ");
-    for (int pos = 0; pos < 16 ;++pos) {
-        printf("%02X", devinfo.serialnum[pos]);
-    }
+   // print out the device serial number, firmware and hardware version number..
+   printf("RPLIDAR S/N: ");
+   for (int pos = 0; pos < 16; ++pos) {
+      printf("%02X", devinfo.serialnum[pos]);
+   }
 
-    printf("\n"
-            "Firmware Ver: %d.%02d\n"
-            "Hardware Rev: %d\n"
-            , devinfo.firmware_version>>8
-            , devinfo.firmware_version & 0xFF
-            , (int)devinfo.hardware_version);
+   printf("\n"
+      "Firmware Ver: %d.%02d\n"
+      "Hardware Rev: %d\n"
+      , devinfo.firmware_version >> 8
+      , devinfo.firmware_version & 0xFF
+      , (int)devinfo.hardware_version);
 
 
 
-    // check health...
-    if (!checkRPLIDARHealth(drv)) {
-        goto on_finished;
-    }
+   // check health...
+   if (!checkRPLIDARHealth(drv)) {
+      goto on_finished;
+   }
 
-    signal(SIGINT, ctrlc);
-    
-    drv->startMotor();
-    // start scan...
-    drv->startScan(0,1);
+   signal(SIGINT, ctrlc);
 
-    // fetech result and print it out...
-    while (1) {
-        rplidar_response_measurement_node_hq_t nodes[8192];
-        size_t   count = _countof(nodes);
+   drv->startMotor();
+   // start scan...
+   drv->startScan(0, 1);
 
-        op_result = drv->grabScanDataHq(nodes, count);
-        if (IS_OK(op_result)) {
-            drv->ascendScanData(nodes, count);
-            for (int pos = 0; pos < (int)count ; ++pos) {
-                printf("%s theta: %03.2f Dist: %08.2f Q: %d \n", 
-                    (nodes[pos].flag & RPLIDAR_RESP_MEASUREMENT_SYNCBIT) ?"S ":"  ", 
-                    (nodes[pos].angle_z_q14 * 90.f / (1 << 14)), 
-                    nodes[pos].dist_mm_q2/4.0f,
-                    nodes[pos].quality);
-            }
-        }
-
-        if (ctrl_c_pressed){ 
+   try {
+      // listener socket
+      ServerSocket server(30000);
+      // make socket
+      ServerSocket sock;
+      // wait until server accepts socket 
+      while (true) {
+         if (server.accept(sock)) {
             break;
-        }
-    }
+         }
+      }
 
-    drv->stop();
-    drv->stopMotor();
-    // done!
+      // start original code...
+      // fetch result and print it out...
+      _u32 positions[360][3];
+      while (1) {
+         rplidar_response_measurement_node_hq_t nodes[8192];
+         size_t   count = _countof(nodes);
+
+         op_result = drv->grabScanDataHq(nodes, count);
+
+         if (IS_OK(op_result)) {
+            drv->ascendScanData(nodes, count);
+            for (int pos = 0; pos < (int)count; ++pos) {
+               _u32 a = nodes[pos].angle_z_q14 * 90.f / (1 << 14);
+               _u32 b = nodes[pos].dist_mm_q2 / 4.0f;
+               _u32 c = nodes[pos].quality;/*
+                   printf("%s theta: %03.2f Dist: %08.2f Q: %d \n",
+                       (nodes[pos].flag & RPLIDAR_RESP_MEASUREMENT_SYNCBIT) ?"S ":"  ", a, b, c);*/
+               positions[pos][0] = a;
+               positions[pos][1] = b;
+               positions[pos][2] = c;
+               // send data to client
+               try {
+                  sock << std::to_string(a) << "," << std::to_string(b) << "," << std::to_string(c) << "\n";
+               }
+               catch (SocketException e) {
+                  std::cout << "lost connection with client, waiting for reconnection...\n";
+                  while (true) {
+                     if (server.accept(sock)) {
+                        break;
+                     }
+                  }
+               }
+            }
+         }
+
+         if (ctrl_c_pressed) {
+            break;
+         }
+      }
+      // end original code
+
+
+   }
+   catch (SocketException & e) {
+      std::cout << "Exception was caught:" << e.description() << "\nExiting!";
+      return 1;
+   }
+
+   drv->stop();
+   drv->stopMotor();
+   // done!
 on_finished:
-    RPlidarDriver::DisposeDriver(drv);
-    drv = NULL;
-    return 0;
+   RPlidarDriver::DisposeDriver(drv);
+   drv = NULL;
+   return 0;
 }
 
