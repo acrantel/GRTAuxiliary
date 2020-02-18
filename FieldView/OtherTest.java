dcb29ff8bcad58c523f0eb46b417d8eea043f712
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2020-02-17 22:54:22 -0800

    add fieldview

---
+++ FieldView/OtherTest.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import com.google.gson.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class OtherTest {
+    public static void main(String args[]) {
+        String buttonClick = PostClass.goGet("buttondata");
+        String canvasClick = PostClass.goGet("canvasdata");
+        System.out.println("Button clicked: " + buttonClick.replace("\n", ""));
+        System.out.println("Canvas spot clicked: " + Arrays.toString(canvasClick.replace("\n", "").substring(1,canvasClick.length()-2).split(":|,")));
+
+        PostClass.goPost("starttimer", "true");
+
+        HashMap<String, Integer> working = new HashMap<>();
+        working.put("fl", 0);
+        working.put("fr", 1);
+        working.put("bl", 1);
+        working.put("br", 1);
+        PostClass.goPost("swervedata", new Gson().toJson(working));
+
+        PostClass.goPost("lemondata", "4");
+
+        ArrayList<int[]> points = new ArrayList<>();
+        points.add(new int[] { 1, 1 });
+        points.add(new int[] { 100, 100 });
+        points.add(new int[] {200,0});
+        points.add(new int[] {500,300});
+        PostClass.goPost("getlidar", new Gson().toJson(points));
+
+        for (int i = 0; i < 360; i++) {
+            PostClass.goPost("angledata", Integer.toString(i));
+            try {
+                Thread.sleep(25);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ FieldView/static/config.js
@@ -0,0 +1,2 @@
+//const ip = "10.1.92.151"
+const ip = "localhost"
